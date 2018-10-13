e7538cf05fb642f9fd9d4645c16895528d10f68e
GitHubAlexTroy <podliza123@gmail.com>
2018-10-13 18:28:39 +0300

    test

---
+++ app/src/main/java/com/example/system/testroom/adapter/DbAdapter.java
@@ -0,0 +1,82 @@
+package com.example.system.testroom.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.system.testroom.R;
+import com.example.system.testroom.bd.App;
+import com.example.system.testroom.bd.AppDatabase;
+import com.example.system.testroom.bd.EmployeeDao;
+import com.example.system.testroom.model.Employee;
+
+import java.util.List;
+
+import io.reactivex.Completable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+public class DbAdapter extends RecyclerView.Adapter<DbAdapter.ViewHolder> {
+
+    private List<Employee> mEmployees;
+
+    public DbAdapter(List<Employee> employees) {
+        mEmployees = employees;
+    }
+
+    @NonNull
+    @Override
+    public DbAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.inventory_item, viewGroup, false);
+        return new DbAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DbAdapter.ViewHolder holder, int position) {
+        Employee employee = mEmployees.get(position);
+        holder.name.setText(employee.getName());
+        holder.salary.setText(String.valueOf(employee.getSalary()));
+
+        holder.delete.setOnClickListener(view -> {
+            AppDatabase db = App.getInstance().getDatabase();
+            EmployeeDao employeeDao = db.employeeDao();
+
+            Completable.fromAction(() -> employeeDao
+                    .delete(employee))
+                    .subscribeOn(Schedulers.io())
+                    .subscribe();
+
+            employeeDao.getAll()
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(this::setData);
+        });
+    }
+
+    public void setData(List<Employee> newData) {
+        mEmployees = newData;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return mEmployees.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView name;
+        private TextView salary;
+        private Button delete;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.product_name);
+            salary = itemView.findViewById(R.id.product_price);
+            delete = itemView.findViewById(R.id.delete_btn);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/system/testroom/view/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.system.testroom.view;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.system.testroom.R;
+import com.example.system.testroom.adapter.DbAdapter;
+import com.example.system.testroom.bd.App;
+import com.example.system.testroom.bd.AppDatabase;
+import com.example.system.testroom.bd.EmployeeDao;
+import com.example.system.testroom.model.Employee;
+
+import java.util.List;
+
+import io.reactivex.Completable;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DbAdapter adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inventory_list);
+
+        bd();
+    }
+
+    private void bd() {
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        FloatingActionButton floatingActionButton = findViewById(R.id.fab);
+
+        AppDatabase db = App.getInstance().getDatabase();
+        EmployeeDao employeeDao = db.employeeDao();
+
+
+        adapter = new DbAdapter(null);
+
+        employeeDao.getAll()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(newData -> {
+                    adapter.setData(newData);
+                });
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView.setAdapter(adapter);
+
+        floatingActionButton.setOnClickListener(view -> {
+            Employee employee = new Employee();
+            employee.setName("Alex Troy");
+            employee.setSalary(23);
+
+            Completable.fromAction(() -> employeeDao
+                    .insert(employee))
+                    .subscribeOn(Schedulers.io())
+                    .subscribe();
+
+            employeeDao.getAll()
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(newData -> {
+
+                        adapter.setData(newData);
+                    });
+        });
+
+    }
+
+}
